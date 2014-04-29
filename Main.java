979a4541c97c24f227fe6ab7c5b5181e76195bb9
jpcastillo <cscpcc.jcastillo@gmail.com>
2014-04-29 05:33:03 -0700

    Messing with downloading HTML pages and writing to a local file

--- Main.java
+++ Main.java
@@ -2,17 +2,27 @@ import java.io.FileOutputStream;
 import java.io.FileNotFoundException;
 import java.io.UnsupportedEncodingException;
 import java.io.PrintWriter;
+import java.net.URL;
+import java.net.URLConnection;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.io.IOException;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.BufferedWriter;
 
 /*
-	Class to be used for writing to files, logging files crawled
+	Class to be used for writing to files. This class overwrites
+	existing files and create non-existing files
 */
-class Logger {
-	PrintWriter log_writer;
-	String logfile;
-	public Logger(String _logfile) {
-		logfile = _logfile;
+class HTMLWriter {
+	PrintWriter writer;
+	String file;
+	public HTMLWriter(String _file) {
+		file = _file;
 		try {
-			log_writer = new PrintWriter(logfile,"UTF-8");
+			writer = new PrintWriter(file,"UTF-8");
 		}
 		catch(FileNotFoundException fnfe) {
 			System.out.println("File not found: " + fnfe.getMessage());
@@ -22,10 +32,84 @@ class Logger {
 		}
 	}
 	public void close() {
-		log_writer.close();
+		writer.close();
 	}
 	public void write(String str) {
-		log_writer.println(str);
+		writer.println(str);
+	}
+}
+
+/*
+	Class for crawling the web
+*/
+class Crawler {
+  	String seedfile, outputdir;
+
+  	public Crawler(String _seedfile, String _outputdir) {
+  		seedfile=_seedfile;
+  		outputdir=_outputdir;
+  	}
+	public void start() {
+		try {
+			URL url = new URL("http://www.ucr.edu");
+			URLConnection connection = url.openConnection();
+			BufferedReader buffin = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+			String html = "", line;
+			while ((line = buffin.readLine()) != null) {
+			   html += line + System.getProperty("line.separator");
+			}
+			//System.out.println(html);
+		  	HTMLWriter writer = new HTMLWriter(outputdir+"/"+"www.ucr.edu");
+		  	writer.write(html);
+		  	writer.close();
+		}
+		catch(MalformedURLException mue) {
+			System.out.println("Bad URL: "+mue.getMessage());
+		}
+		catch(IOException ioe) {
+			System.out.println("IOException: "+ioe.getMessage());
+		}
+	}
+}
+
+/*
+	This class is set up to append strings to files. Perfect for
+	keeping a history of crawled pages.
+*/
+class Logger {
+	BufferedWriter fout;
+	File dir;
+	String abspath, file;
+	FileWriter fstream;
+
+	public Logger(String _file) {
+		file = _file;
+		try {
+			dir = new File(".");
+			abspath = dir.getCanonicalPath() + File.separator + file;
+			fstream = new FileWriter(abspath, true);
+			fout = new BufferedWriter(fstream);
+		}
+		catch(IOException ioe) {
+			System.out.println("IOException: "+ioe.getMessage());
+		}
+	}
+	public void close() {
+		try {
+			fout.close();
+		}
+		catch(IOException ioe) {
+			System.out.println("IOException: "+ioe.getMessage());
+		}
+	}
+	public void write(String str) {
+		try {
+			fout.write(str);
+			fout.newLine();
+		}
+		catch(IOException ioe) {
+			System.out.println("IOException: "+ioe.getMessage());
+		}
 	}
 }
 
@@ -34,10 +118,12 @@ class Logger {
 */
 public class Main {
 	public static void main(String[] args) {
-		/*System.out.println("Number of Args is "+args.length+" and they are:");
+		/*
+		System.out.println("Number of Args is "+args.length+" and they are:");
 		for (String s: args) {
           System.out.println(s);
-      	}*/
+      	}
+      	*/
       	String crawlerlog = "filescrawled.txt";
       	String seedfile = args[0];
       	int numpages = Integer.parseInt(args[1]);
@@ -47,10 +133,11 @@ public class Main {
       	String pme = "Seed File: "+seedfile+"\nNumber of Pages: "+numpages+"\nNumber of Hops Away: "+hopsaway+"\nOutput Directory: "+outputdir;
       	System.out.println(pme);
 
-      	// Let's test the logging class and methods
       	Logger log = new Logger(crawlerlog);
       	log.write(pme);
-      	log.close();
+
+      	Crawler spidey = new Crawler(seedfile,outputdir);
+      	spidey.start();
 	}
 }
 
